e46e4223cb8da5ded2a6fa587f219682b9519477
Geovana Reis <geovanareiss@hotmail.com>
2026-09-10 16:58:20 -0300

    Add files via upload

---
+++ exercicio16_idadeMedia.js
@@ -0,0 +1,11 @@
+let idades = [50, 36, 100, 15, 28, 47];
+let soma = 0;
+
+for (let numero = 0; numero < idades.length; numero ++){
+    soma += idades[numero];
+}
+    let media = soma / idades.length;
+    console.log(media);
+      
+
+
